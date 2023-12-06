54c8d10ebb451c776be9e538f7dde3fe5700348f
jiangruijian <jrj444@foxmail.com>
2023-12-06 18:03:18 +0800

    hw0b: 完成 MapExercises

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,23 +1,37 @@
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapExercises {
     /** Returns a map from every lower case letter to the number corresponding to that letter, where 'a' is
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        return null;
+        Map<Character, Integer> map = new TreeMap<>();
+        for (int i = 0; i < 26; i++) {
+            map.put((char)('a' + i), i + 1);
+        }
+        return map;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        return null;
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (Integer integer : nums) {
+            map.put(integer, integer * integer);
+        }
+        return map;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        return null;
+        Map<String, Integer> map = new TreeMap<>();
+        for (String s : words) {
+            Integer count = map.get(s);
+            map.put(s, count == null ? 1 : count + 1);
+        }
+        return map;
     }
 }
