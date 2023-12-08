8a43fe24012f2fe5f48d5811c34738966b0d57f3
jiangruijian <jrj444@foxmail.com>
2023-12-08 21:13:22 +0800

    完成 lab02 BombMain 实验

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -3,6 +3,15 @@ package bomb;
 import common.IntList;
 
 public class BombMain {
+    public static String toString(IntList list) {
+        if (list.rest == null) {
+            // Converts an Integer to a String!
+            return String.valueOf(list.first);
+        } else {
+            return list.first + " " + toString(list.rest);
+        }
+    }
+
     public static void main(String[] args) {
         int phase = 2;
         if (args.length > 0) {
@@ -11,13 +20,18 @@ public class BombMain {
         // TODO: Find the correct passwords to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1(IntList.of(0, 9, 3, 0, 8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            IntList list = new IntList(0, null);
+            for (int i = 0; i < 1338; i++) {
+                list = new IntList(-81201430, list);
+            }
+            String password = toString(list);
+            b.phase2(password);
         }
     }
 }
